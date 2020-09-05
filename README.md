611f18e46a02fbb74815e71fdfc7b1dd2fda8ee6
DanielTemesgen <40549299+DanielTemesgen@users.noreply.github.com>
2020-09-06 00:38:08 +0100

    added a readme and cleaned the code by adding docstrings and functions.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Statistical Calculator Frontend
+This repo uses streamlit to create a frontend for a simple API I created and hosted on AWS Lambda [here](https://github.com/DanielTemesgen/aws_statistical_calculator).
+
+The streamlit is hosted on mybinder.
+This is not worth an EC2 instance (or the trouble it would take to make it temporary) and streamlit doesn't currently support serverless deployments.
+
+This builds on a POC proving the mybinder-streamlit link [here](https://github.com/chekos/testing-streamlit-mybinder).
+
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/DanielTemesgen/statistical_calculator_frontend/master?urlpath=proxy/8501/)
+
+![image](assets/streamlit.png)
+
+## Components
+Streamlit is great for quickly knocking up a frontend from a python script, it supports caching too which helps save on API calls. <br>
+The requests library is used to form the API get request. <br>
+Streamlit can catch errors and display them to the user, which helps for when 
--- main.py
+++ main.py
@@ -5,7 +5,15 @@ import requests
 
 
 @st.cache
-def calculate(numbers: List[float]):
+def calculate(numbers: List[float]) -> requests.Response:
+    """Performs the statistical calculation by sending in the array of numbers to the API.
+
+    Args:
+        numbers: list of numbers to send to the API.
+
+    Returns:
+        a requests response object that can be queried.
+    """
     url = 'https://7z8boxyi92.execute-api.us-east-1.amazonaws.com'
     endpoint = '/prod/stats/'
     query = {'numbers': numbers}
@@ -13,11 +21,27 @@ def calculate(numbers: List[float]):
     return r
 
 
+def display(numbers: List[float]):
+    """Display a success message if the API has a positive status and an error if not.
+    Either way the response is displayed.
+
+    Args:
+        numbers:
+    """
+    response = calculate(numbers)
+    if response.status_code == 200:
+        st.success("The statistical description:")
+    else:
+        st.error('API Error (see below)')
+
+    st.write(response.json())
+
+
 st.write("# Statistical Calculator")
 
 raw_num = st.text_area(
     label='Insert a list of numbers separated by a comma.',
-    value='1,2,3',
+    value='1,2,3,42',
     height=10)
 
 try:
@@ -25,5 +49,4 @@ try:
 except ValueError as e:
     st.error('Insert a list of numbers separated by a comma.')
 else:
-    st.success("The statistical description:")
-    st.write(calculate(parsed_numbers).json())
+    display(parsed_numbers)
